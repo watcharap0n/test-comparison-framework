bcddf0d363ee7ddfff04aa88a2fed7629abfe33d
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2023-07-19 21:08:36 +0700

    update mockserver

--- unit-test-service/tests_pytest/conftest.py
+++ unit-test-service/tests_pytest/conftest.py
@@ -10,10 +10,10 @@ import os
 import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
+from mongomock import MongoClient
 from server.main import app
 
 LOGGER = logging.getLogger(__name__)
-client = TestClient(app)
 payload = {
     'username': 'dev',
     'firstname': 'watcharapon',
@@ -21,10 +21,19 @@ payload = {
 }
 
 
-@pytest.fixture
-def get_home_page():
-    response = client.get('/')
-    return response.json()
+@pytest.fixture(scope="module")
+def mock_mongo_client():
+    # Create a mock MongoDB client using mongomock
+    client_db = MongoClient()
+    yield client_db
+    # Cleanup after all tests are executed
+    client_db.close()
+
+
+@pytest.fixture(scope="module")
+def client(mock_mongo_client):
+    # Use the TestClient with the FastAPI app
+    return TestClient(app)
 
 
 @pytest.fixture
@@ -33,7 +42,7 @@ def authorization_header():
 
 
 @pytest.fixture
-def created_user_by_id(authorization_header):
+def created_user_by_id(client, authorization_header):
     response = client.post('/user/create', json=payload, headers=authorization_header)
     created_id = response.json()['_id']
     yield created_id
@@ -42,19 +51,19 @@ def created_user_by_id(authorization_header):
     client.delete(f'/user/delete/{created_id}', headers=authorization_header)
 
 
-def test_create_success(created_user_by_id, authorization_header):
+def test_create_success(client, created_user_by_id, authorization_header):
     response = client.get(f'/user/find/{created_user_by_id}', headers=authorization_header)
     assert response.status_code == status.HTTP_200_OK
 
 
 # testing method post status code 400
-def test_create_duplicate(created_user_by_id, authorization_header):
+def test_create_duplicate(client, created_user_by_id, authorization_header):
     response = client.post('/user/create', json=payload, headers=authorization_header)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
 # testing method post status code 422
-def test_create_user_unprocessable(authorization_header):
+def test_create_user_unprocessable(client, authorization_header):
     n_payload = payload.copy()
     del n_payload['username']
     response = client.post('/user/create', json=n_payload, headers=authorization_header)
@@ -62,19 +71,19 @@ def test_create_user_unprocessable(authorization_header):
 
 
 # testing method put status code 200
-def test_update_user_success(created_user_by_id, authorization_header):
+def test_update_user_success(client, created_user_by_id, authorization_header):
     payload['username'] = 'kane'
     response = client.put(f'/user/update/{created_user_by_id}', json=payload, headers=authorization_header)
     assert response.status_code == status.HTTP_200_OK
 
 
 # testing method put status code 400
-def test_update_user_duplicate(created_user_by_id, authorization_header):
+def test_update_user_duplicate(client, created_user_by_id, authorization_header):
     payload['username'] = 'kane'
     response = client.put(f'/user/update/{created_user_by_id}', json=payload, headers=authorization_header)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
-def test_delete_user(created_user_by_id, authorization_header):
+def test_delete_user(client, created_user_by_id, authorization_header):
     response = client.delete(f'/user/delete/{created_user_by_id}', headers=authorization_header)
     assert response.status_code == status.HTTP_204_NO_CONTENT
